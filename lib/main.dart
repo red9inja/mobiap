5aa1e77e7776aac4d2d4c592aeb31bf13be1bc4a
root <root@ip-172-31-36-132.ap-southeast-2.compute.internal>
2025-02-18 10:16:03 +0000

    check2f

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You check vishwa have pushed the button this many times:',
+              'You vishwa have pushed the button this many times:',
             ),
             Text(
               '$_counter',
